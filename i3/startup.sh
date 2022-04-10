434a6094982860c6445be11b208f00d937bd3c63
Gary Yendell <gary.yendell@diamond.ac.uk>
2022-04-10 10:05:35 +0100

    Move things from i3 config to startup.sh

--- i3/startup.sh
+++ i3/startup.sh
@@ -2,13 +2,19 @@
 
 ## Main + Secondary on top
 # xrandr --output DisplayPort-0 --pos 0x1080 --output HDMI-A-0 --pos 960x0 --output HDMI-A-1 --same-as DisplayPort-0
+xrandr --output DisplayPort-0 --pos 0x1080 --output HDMI-A-0 --pos 960x0 --output HDMI-A-1 --off
 
 # Enable Status Bar
-# ~/dotfiles/i3/polybar.sh
+~/dotfiles/i3/polybar.sh
 
 # Set Background
-# feh --bg-fill ~/Pictures/HomerStarrySky.jpg
+feh --bg-fill ~/Pictures/HomerStarrySky.jpg
 
 # Enable Compositor
-# picom -b -c --active-opacity 0.98 --inactive-opacity 0.95 --backend GLX --vsync --config ~/.config/picom.conf
+picom -b -c --active-opacity 0.98 --inactive-opacity 0.95 --backend GLX --vsync --config ~/.config/picom.conf
 
+# Start xscreensaver
+xscreensaver -no-splash
+
+# Mouse sensitivity
+xinput --set-prop "Logitech G502 HERO Gaming Mouse" "libinput Accel Speed" 1
