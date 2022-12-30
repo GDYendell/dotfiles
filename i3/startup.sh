9b7f0314918d07c9de1d368a3704773d9dab51c1
Gary Yendell <gary.yendell@diamond.ac.uk>
2022-12-30 19:50:28 +0000

    i3: Fix startup

--- i3/startup.sh
+++ i3/startup.sh
@@ -8,17 +8,17 @@
 # xrandr --output DP-0 --mode 3440x1440 --pos 0x0 --rotate normal --panning 3440x1440 --primary --output DP-2 --mode 1920x1200 --rotate left --pos 3440x0 --panning 1920x1200
 
 # Enable Status Bar
-~/dotfiles/i3/polybar.sh
+# ~/dotfiles/i3/polybar.sh
 
 # Set Background
 # feh --bg-fill ~/Pictures/HomerStarrySky.jpg
 # display -window root ~/Pictures/Wallpapers/Nebula3.jpg
 
 # Enable Compositor
-picom -b -c --active-opacity 0.98 --inactive-opacity 0.95 --backend GLX --vsync --config ~/dotfiles/config/picom
+# picom -b -c --active-opacity 0.98 --inactive-opacity 0.95 --backend GLX --vsync --config ~/dotfiles/config/picom
 
 # Run xidlehook for screen timeout and suspend
-#./idle.sh
+# ~/dotfiles/i3/idle.sh
 
 # Run conky
 # conky
