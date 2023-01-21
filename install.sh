952ad8208c12836350ad03bcec755afcdfa4530b
Gary Yendell <gary.yendell@diamond.ac.uk>
2023-01-21 16:28:35 +0000

    Update install script

--- install.sh
+++ install.sh
@@ -2,7 +2,15 @@
 
 ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/inputrc ~/.inputrc
+ln -s ~/dotfiles/profile ~/.profile
 ln -s ~/dotfiles/git/config ~/.gitconfig
-ln -s ~/dotfiles/config/i3 ~/.config/i3/config
+ln -s ~/dotfiles/ssh/config ~/.ssh/config
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/xinitrc ~/.xinitrc
+
 ln -s ~/dotfiles/config/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -s ~/dotfiles/config/fonts/fonts.conf ~/.config/fontconfig/fonts.conf
+ln -s ~/dotfiles/config/i3 ~/.config/i3/config
 ln -s ~/dotfiles/config/polybar ~/.config/polybar/config
+
+ln -s ~/dotfiles/applications/obsidian.desktop ~/.local/share/applications/obsidian.desktop
