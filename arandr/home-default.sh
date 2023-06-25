03f669c0b4f0f3f77d8facfe5e690fa21e8d33a3
Gary Yendell <gary.yendell@diamond.ac.uk>
2023-06-25 08:21:37 +0100

    Update home monitor layouts

--- arandr/home-default.sh
+++ arandr/home-default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --primary --mode 3840x2160 --pos 0x1080 --rotate normal --output DisplayPort-1 --off --output HDMI-A-0 --mode 1920x1080 --pos 960x0 --rotate normal --output HDMI-A-1 --off --output DVI-D-0 --off
+xrandr --output DisplayPort-0 --primary --mode 3840x2160 --pos 0x1080 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --off --output HDMI-A-0 --off --output HDMI-A-1-1 --mode 1920x1080 --pos 960x0 --rotate normal --output DisplayPort-1-3 --off --output DisplayPort-1-4 --off --output DisplayPort-1-5 --off
--- arandr/home-tv.sh
+++ arandr/home-tv.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output HDMI-A-0 --off --output HDMI-A-1 --mode 3840x2160 --pos 0x0 --rotate normal --output DVI-D-0 --off
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output HDMI-A-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-A-1-1 --off --output DisplayPort-1-3 --off --output DisplayPort-1-4 --off --output DisplayPort-1-5 --off
