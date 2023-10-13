a806198b7e661bda81752461e8078cba8f1e58ee
Gary Yendell <gary.yendell@diamond.ac.uk>
2023-10-13 08:43:41 +0000

    Use sudo and ensure ~/.config exists

--- vscode/install.sh
+++ vscode/install.sh
@@ -2,13 +2,18 @@
 
 DISTRO=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
 
+# Run commands with sudo if container user is not root
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    SUDO="sudo"
+fi
+
 # Don't pollute workspace with downloaded files
 cd /tmp
 
 # Tools
 if [[ "$DISTRO" =~ Debian ]]; then
-    sudo apt-get update
-    sudo apt-get install -y vim
+    ${SUDO} apt-get update
+    ${SUDO} apt-get install -y vim
 fi
 
 # Lazygit
@@ -16,14 +21,15 @@ if [[ "$DISTRO" =~ Debian ]]; then
     LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
     curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
     tar xf lazygit.tar.gz lazygit
-    sudo install lazygit /usr/local/bin
+    ${SUDO} install lazygit /usr/local/bin
 elif [[ "$DISTRO" =~ CentOS ]]; then
-    dnf copr enable atim/lazygit -y
-    dnf install lazygit
+    ${SUDO} dnf copr enable atim/lazygit -y
+    ${SUDO} dnf install lazygit
 fi
 
 # Starship Prompt
 curl -sS https://starship.rs/install.sh | sh -s -- -y
+mkdir -p ~/.config
 ln -s ~/dotfiles/config/starship.toml ~/.config/starship.toml
 
 echo -e '\n. ~/dotfiles/bashrc' >> ~/.bashrc
