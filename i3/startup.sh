83ca3404df1343677614da05b315a44e93fc73cf
Gary Yendell <gary.yendell@diamond.ac.uk>
2024-03-27 19:56:47 +0000

    Tidy i3 startup

--- i3/startup.sh
+++ i3/startup.sh
@@ -1,11 +1,6 @@
 # Monitor layout
-
-## Monitor Layout
-# Main + Secondary on top
-# xrandr --output DisplayPort-0 --pos 0x1080 --output HDMI-A-0 --pos 960x0 --output HDMI-A-1 --same-as DisplayPort-0
-# xrandr --output DisplayPort-0 --pos 0x1080 --output HDMI-A-0 --pos 960x0 --output HDMI-A-1 --off
-# Work | Main + Secondary Right
-# xrandr --output DP-0 --mode 3440x1440 --pos 0x0 --rotate normal --panning 3440x1440 --primary --output DP-2 --mode 1920x1200 --rotate left --pos 3440x0 --panning 1920x1200
+# ~/arandr/home-default.sh
+# ~/arandr/work.sh
 
 # Keybindings
 xmodmap ~/.Xmodmap
